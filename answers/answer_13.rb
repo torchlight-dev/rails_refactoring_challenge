e605dc0ce24c94f5c6b5e86d30475b54a8baee45
shohei.akimoto <shohei.akimoto@torchlight.co.jp>
2018-09-27 16:36:52 +0900

    answered q13

---
+++ answers/answer_13.rb
@@ -0,0 +1,13 @@
+class Answer12
+
+  DATA = [1,2,3,4,5]
+  TARGETS = [3,6]
+
+  def self.execute
+    target = TARGETS.sample
+    pp DATA.any? { |data| data > target }
+  end
+
+end
+
+Answer12.execute
