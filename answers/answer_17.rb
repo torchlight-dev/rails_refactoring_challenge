683b2b3000f654efc486ac395a18a7166721b3ac
shohei.akimoto <shohei.akimoto@torchlight.co.jp>
2018-09-28 13:44:37 +0900

    modified q17 using methods

--- answers/answer_17.rb
+++ answers/answer_17.rb
@@ -2,7 +2,9 @@ class Answer17
 
   def self.execute
     users = User.all
-    pp users.select { |user| user[:age] > 20 }.select { |user| user.bmi > 25 }.map(&:name)
+    pp users.select { |user| user.is_adult && user.is_fat }.map(&:name)
+    # 初期に書いた別解答
+    # pp users.select { |user| user[:age] > 20 }.select { |user| user.bmi > 25 }.map(&:name)
   end
 
 end
