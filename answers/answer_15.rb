c5c4cd693f15c725644291d629ccbdcea6e5adf1
shohei.akimoto <shohei.akimoto@torchlight.co.jp>
2018-09-28 16:21:44 +0900

    modified cause of User class

--- answers/answer_15.rb
+++ answers/answer_15.rb
@@ -2,7 +2,9 @@ class Answer15
 
   def self.execute
     user = User.find(1)
-    pp user.as_json(methods: 'bmi')
+    pp user.with_bmi
+    # 修正前
+    # pp user.as_json(methods: 'bmi')
   end
 
 end
--- answers/answer_16.rb
+++ answers/answer_16.rb
@@ -2,7 +2,9 @@ class Answer16
 
   def self.execute
     users = User.all
-    pp users.map { |user| user.as_json(methods: 'bmi') }
+    pp users.map(&:with_bmi)
+    # 修正前
+    # pp users.map { |user| user.as_json(methods: 'bmi') }
   end
 
 end
